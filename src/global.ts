c366bb69d04bb758336b0086f1c2733de79be673
skyclouds2001 <skyclouds2001@163.com>
2024-10-08 18:39:36 +0800

    perf: move away old code

--- src/global.ts
+++ src/global.ts
@@ -1,11 +1,1 @@
 /// <reference types="./../lib/index.d.ts" />
-
-import type { Router } from './types'
-
-export const global: Global = {
-  router: null
-}
-
-export interface Global {
-  router: Router | null
-}
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,1 @@
 /// <reference types="./../lib/index.d.ts" />
-
-export interface Router {
-  readonly options: Readonly<RouterOptions>
-  back: () => void
-  forward: () => void
-  go: (delta: number) => void
-}
-
-export interface RouterOptions {
-  routes: ReadonlyArray<RouteRecord>
-}
-
-export interface RouteRecord {
-  path: string
-  component: Node
-}
