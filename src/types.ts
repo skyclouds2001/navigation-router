6938e31d6a056aac742ed8dca256da908ee5cde9
skyclouds2001 <skyclouds2001@163.com>
2024-10-13 01:22:18 +0800

    feat: reorganize types

--- src/types.ts
+++ src/types.ts
@@ -3,13 +3,15 @@
 import type { RouterLink } from './router-link'
 import type { RouterView } from './router-view'
 
-interface RouterRecordSingleView {
+interface RouterRecordBase {
   path: string
+}
+
+interface RouterRecordSingleView extends RouterRecordBase {
   component: Node
 }
 
-interface RouterRecordMultipleView {
-  path: string
+interface RouterRecordMultipleView extends RouterRecordBase {
   components: Record<string, Node>
 }
 
@@ -19,9 +21,16 @@ export interface RouterOptions {
   routes: RouterRecordRaw[]
 }
 
+type RouterEventMap = Record<string, Event>
+
 export interface Router extends EventTarget {
   readonly $navigation: Navigation
   readonly $options: Readonly<RouterOptions>
   readonly $views: Set<RouterView>
   readonly $links: Set<RouterLink>
+
+  addEventListener<K extends keyof RouterEventMap>(type: K, listener: (this: Router, ev: RouterEventMap[K]) => unknown, options?: boolean | AddEventListenerOptions): void
+  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void
+  removeEventListener<K extends keyof RouterEventMap>(type: K, listener: (this: Router, ev: RouterEventMap[K]) => unknown, options?: boolean | EventListenerOptions): void
+  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void
 }
