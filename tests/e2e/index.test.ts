b8fa9795d93909f8a4645bcb8898c331803576fd
skyclouds2001 <skyclouds2001@163.com>
2024-11-13 22:08:25 +0800

    test: add test for router-view & router-link

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,7 +1,43 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/')
+})
 
+test('page should be able to load', async ({ page }) => {
   await expect(page).toHaveTitle('navigation-router')
+
+  await expect(page).toHaveURL('/')
+
+  await expect(page.getByTestId('view:').getByText('home', { exact: true })).toHaveText('home')
+})
+
+test('router-link should able to navigate', async ({ page }) => {
+  await page.getByTestId('link:a').click()
+
+  await expect(page).toHaveURL('/a')
+
+  await page.getByTestId('link:b').click()
+
+  await expect(page).toHaveURL('/b')
+})
+
+test('router-view should able to load component', async ({ page }) => {
+  await page.getByTestId('link:a').click()
+
+  await expect(page.getByTestId('view:').getByText('a', { exact: true })).toHaveText('a')
+
+  await page.getByTestId('link:b').click()
+
+  await expect(page.getByTestId('view:').getByText('b', { exact: true })).toHaveText('b')
+})
+
+test('router-view should support named router', async ({ page }) => {
+  await page.getByTestId('link:c').click()
+
+  await expect(page.getByTestId('view:').getByText('c', { exact: true })).toHaveText('c')
+
+  await expect(page.getByTestId('view:c1').getByText('c1', { exact: true })).toHaveText('c1')
+
+  await expect(page.getByTestId('view:c2').getByText('c2', { exact: true })).toHaveText('c2')
 })
