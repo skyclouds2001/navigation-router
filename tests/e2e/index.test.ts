5e8cff45605966f8d1e109eaa5f4ff5515c322c4
skyclouds2001 <skyclouds2001@163.com>
2024-11-13 22:57:26 +0800

    test: add test for router link class

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -22,6 +22,26 @@ test('router-link should able to navigate', async ({ page }) => {
   await expect(page).toHaveURL('/b')
 })
 
+test('router-link should able to load class name accordingly', async ({ page }) => {
+  const fs = await import('node:fs')
+  const str = await page.getByTestId('link:home').evaluate((el) => el.outerHTML)
+  fs.appendFileSync('log.txt', (str ?? 'unknown') + '\n')
+
+  await expect(page.getByTestId('link:home')).toHaveClass('router-link-active router-link-exact-active')
+
+  await expect(page.getByTestId('link:a')).toHaveClass('')
+
+  await expect(page.getByTestId('link:b')).toHaveClass('')
+
+  await page.getByTestId('link:a').click()
+
+  await expect(page.getByTestId('link:home')).toHaveClass('router-link-active')
+
+  await expect(page.getByTestId('link:a')).toHaveClass(['router-link-active router-link-exact-active'])
+
+  await expect(page.getByTestId('link:b')).toHaveClass('')
+})
+
 test('router-view should able to load component', async ({ page }) => {
   await page.getByTestId('link:a').click()
 
